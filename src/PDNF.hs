b5e6dfc5f325746f4dc5e7e2df3b838b2d267ee4
Jesse Tov <tov@ccs.neu.edu>
2010-05-03 14:15:54 -0400

    pdnf is awesomy

--- src/PDNF.hs
+++ src/PDNF.hs
@@ -3,20 +3,31 @@ module PDNF (
   -- * Abstract representation
   PDNF,
   -- * Construction
-  variable, clause, disjoin, conjoin, disjoinClause, conjoinClause,
+  variable, clause, disjoinClause, conjoinClause,
+  -- * Queries
+  isUnsat, isValid, support,
+  -- ** Assignments
+  Assignment, satisfies, findUnsat,
   -- * Resolution
   assume,
   -- * To and from lists
-  fromLists, fromListsUnsafe, toLists
+  fromLists, fromListsUnsafe, toLists,
+  -- * Tests
+  tests
 ) where
+
+import Syntax.POClass
+import Util
 
 import qualified Data.Set as S
 import Data.List (intersperse)
+import qualified Test.QuickCheck as QC
 
 -- | The type of a Positive DNF over some type 'a'
 newtype PDNF a = PDNF { unPDNF :: [S.Set a] }
 
 -- | Is the formula unsatisfiable?
+-- O(1)
 isUnsat :: PDNF a -> Bool
 isUnsat  = null . unPDNF
 
@@ -26,27 +37,47 @@ isValid  = (== [S.empty]) . unPDNF
 
 -- | To update the formula to reflect an assumption about the
 --   assignment for a particular variable.
-assume  :: Ord a => a -> Bool -> PDNF a -> PDNF a
-assume v True  formula = PDNF . normalize' $
+assume  :: Ord a => Bool -> a -> PDNF a -> PDNF a
+assume True  v formula = PDNF . normalize' $
   map (S.delete v) (unPDNF formula)
-assume v False formula = PDNF $
+assume False v formula = PDNF $
   filter (S.notMember v) (unPDNF formula)
 
 -- | To construct a formula of a single variable
 variable :: a -> PDNF a
 variable  = PDNF . return . S.singleton
 
+-- | To find the support of a PDNF
+support  :: Ord a => PDNF a -> S.Set a
+support   = foldr S.union S.empty . unPDNF
+
 -- | To construct a formula of one conjuction
 clause   :: Ord a => [a] -> PDNF a
 clause    = PDNF . return . S.fromList
 
--- | To find the disjunction of two formulae
-disjoin  :: Ord a => PDNF a -> PDNF a -> PDNF a
-disjoin f = PDNF . foldr disjoinClause' (unPDNF f) . unPDNF where
+instance Ord a => PO (PDNF a) where
+  f1 \/ f2 = PDNF $ foldr disjoinClause' (unPDNF f1) (unPDNF f2)
+  f1 /\ f2 = PDNF $
+    normalize' [ clause1 `S.union` clause2
+               | clause1 <- unPDNF f1
+               , clause2 <- unPDNF f2 ]
+  PDNF ant <: PDNF con
+    = all (\clause -> any (`S.isSubsetOf` clause) con) ant
+
+instance Bounded (PDNF a) where
+  minBound = PDNF []
+  maxBound = PDNF [S.empty]
+
+instance Ord a => Eq (PDNF a) where
+  f1 == f2 = f1 <: f2 && f2 <: f1
 
 -- | To add a clause to a formula
 disjoinClause :: Ord a => [a] -> PDNF a -> PDNF a
 disjoinClause c' = PDNF . disjoinClause' (S.fromList c') . unPDNF
+
+-- | To distribute a clause over a formula
+conjoinClause :: Ord a => [a] -> PDNF a -> PDNF a
+conjoinClause c' = PDNF . conjoinClause' (S.fromList c') . unPDNF
 
 disjoinClause' :: Ord a => S.Set a -> [S.Set a] -> [S.Set a]
 disjoinClause' c' []     = [c']
@@ -57,26 +88,15 @@ disjoinClause' c' (c:cs) =
     then c:cs
     else c:disjoinClause' c' cs
 
--- | To find the conjunction of two formulae
-conjoin :: Ord a => PDNF a -> PDNF a -> PDNF a
-conjoin f1 f2 = PDNF $
-  normalize' [ clause1 `S.union` clause2
-             | clause1 <- unPDNF f1
-             , clause2 <- unPDNF f2 ]
+conjoinClause' :: Ord a => S.Set a -> [S.Set a] -> [S.Set a]
+conjoinClause' c' cs = map (S.union c') cs
 
 normalize' :: Ord a => [S.Set a] -> [S.Set a]
 normalize'  = foldr disjoinClause' []
 
--- | To distribute a clause over a formula
-conjoinClause :: Ord a => [a] -> PDNF a -> PDNF a
-conjoinClause c' = PDNF . conjoinClause' (S.fromList c') . unPDNF
-
-conjoinClause' :: Ord a => S.Set a -> [S.Set a] -> [S.Set a]
-conjoinClause' c' cs = map (S.union c') cs
-
 -- | To construct a PDNF.
 fromLists :: Ord a => [[a]] -> PDNF a
-fromLists  = foldr disjoin minBound . map clause
+fromLists  = foldr (\/) minBound . map clause
 
 -- | To construct a PDNF quickly, assuming that no list is a superset
 --   of an other list.
@@ -100,14 +120,72 @@ instance (Eq a, Show a) => Show (PDNF a) where
                 | lit <- S.toList clause ]
           | clause <- formula ]
 
-instance Bounded (PDNF a) where
-  minBound = PDNF []
-  maxBound = PDNF [S.empty]
+---
+--- Assignments
+---
 
-instance Ord a => Eq (PDNF a) where
-  f1 == f2 = f1 <= f2 && f2 <= f1
+-- | An assignment is a map from variables to booleans, represented
+--   as a list of variables to map to true, with all others mapped
+--   to false.
+type Assignment a = [a]
 
-instance Ord a => Ord (PDNF a) where
-  PDNF ant <= PDNF con
-    = all (\clause -> any (`S.isSubsetOf` clause) con) ant
+-- | Does the given assignment satisfy the PDNF?
+satisfies :: Ord a => PDNF a -> Assignment a -> Bool
+satisfies pdnf vs = isValid (foldr (assume True) pdnf vs)
 
+-- | Find an assignment that satisfies the first PDNF but not
+--   the second.
+findUnsat :: Ord a => PDNF a -> PDNF a -> [Assignment a]
+findUnsat (PDNF f1) (PDNF f2) =
+  [ S.toList clause
+  | clause <- f1
+  , not (any (`S.isSubsetOf` clause) f2) ]
+
+---
+--- Tests
+---
+
+assignFor :: Ord a => PDNF a -> QC.Gen (Assignment a)
+assignFor pdnf =
+  genSublist (S.toList (support pdnf))
+    where
+      genSublist :: [a] -> QC.Gen [a]
+      genSublist lst = do
+        let den = length lst
+        num <- QC.choose (0, den `div` 2)
+        let each rest elt = do
+              pick <- QC.choose (1, den)
+              return $ if pick > num
+                then elt:rest
+                else rest
+        foldM each [] lst
+
+instance (Ord a, QC.Arbitrary a) => QC.Arbitrary (PDNF a) where
+  arbitrary = fromLists `fmap` QC.arbitrary
+  shrink    = map fromLists . QC.shrink . toLists
+
+prop_Impl :: PDNF Int -> PDNF Int -> QC.Property
+prop_Impl f1 f2 =
+  if f1 <: f2 then
+    impl f1 f2
+  else if f2 <: f1 then
+    impl f2 f1
+  else
+    QC.classify True "counterexample" $
+      not (null (findUnsat f1 f2))
+  where impl f1' f2' =
+          QC.classify True "implication" $
+            QC.forAll (assignFor (f1' \/ f2')) $ \s ->
+              satisfies f1' s QC.==> satisfies f2' s
+
+prop_Disj :: PDNF Int -> PDNF Int -> Bool
+prop_Disj f1 f2 = f1 <: f1 \/ f2
+
+prop_Conj :: PDNF Int -> PDNF Int -> Bool
+prop_Conj f1 f2 = f1 /\ f2 <: f1
+
+tests :: IO ()
+tests  = do
+  QC.quickCheck prop_Impl
+  QC.quickCheck prop_Disj
+  QC.quickCheck prop_Conj
