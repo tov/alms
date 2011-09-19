703cefe2b5d7304999f46effd893e6d7519797b4
Jesse A. Tov <jesse.tov@gmail.com>
2011-09-18 23:04:11 -0400

    trying a new way to do session types

--- src/Basis/Channel.hs
+++ src/Basis/Channel.hs
@@ -18,13 +18,13 @@ instance Valuable Channel where
 
 entries :: [Entry Raw]
 entries  = [
-    dec [sgQ| type 'a channel |],
-    fun "new"  -: [ty| all 'a. unit -> 'a channel |]
+    dec [sgQ| type `a channel |],
+    fun "new"  -: [ty| all `a. unit -> `a channel |]
         -= \() -> Channel `fmap` C.newChan,
-    fun "send" -: [ty| all 'a. 'a channel -> 'a -> unit |]
+    fun "send" -: [ty| all `a. `a channel -> `a -> unit |]
         -= \c a -> do
              C.writeChan (unChannel c) a
              return (),
-    fun "recv" -: [ty| all 'a. 'a channel -> 'a |]
+    fun "recv" -: [ty| all `a. `a channel -> `a |]
         -= \c -> C.readChan (unChannel c)
   ]
